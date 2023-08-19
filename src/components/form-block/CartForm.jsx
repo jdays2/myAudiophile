d4984b83f0f5baa645d230b823b31f979ca03c34
Марк <lolka-pop@rambler.ru>
2023-08-20 02:48:26 +0300

    feat: add beta version of form-state

--- src/components/form-block/CartForm.jsx
+++ src/components/form-block/CartForm.jsx
@@ -1,4 +1,25 @@
+import { useState } from 'react';
+
 const CartForm = () => {
+	const [inputState, setInputState] = useState({
+		name: '',
+		email: '',
+		tel: '',
+		adress: '',
+		zip: '',
+		city: '',
+		country: '',
+		money: '',
+	});
+
+	const inputsHandler = (e) => {
+		const { name, value } = e.target;
+		setInputState({
+			...inputState,
+			[name]: value,
+		});
+	};
+	
 	return (
 		<form className="cart-form__main">
 			<p className="cart-form__title">CHECKOUT</p>
@@ -8,6 +29,9 @@ const CartForm = () => {
 					<div className="cart-form__item">
 						<label>Name</label>
 						<input
+							name="name"
+							onChange={inputsHandler}
+							value={inputState.name}
 							type="text"
 							placeholder="Alexei Ward"
 						/>
@@ -16,6 +40,9 @@ const CartForm = () => {
 					<div className="cart-form__item">
 						<label>Email Address</label>
 						<input
+							name="email"
+							onChange={inputsHandler}
+							value={inputState.email}
 							type="text"
 							placeholder="alexei@mail.com"
 						/>
@@ -24,6 +51,9 @@ const CartForm = () => {
 					<div className="cart-form__item">
 						<label>Phone Number</label>
 						<input
+							name="tel"
+							onChange={inputsHandler}
+							value={inputState.tel}
 							type="text"
 							placeholder="+1 202-555-0136"
 						/>
@@ -37,6 +67,9 @@ const CartForm = () => {
 					<div className="cart-form__item cart-form__item--large">
 						<label>Address</label>
 						<input
+							name="adress"
+							onChange={inputsHandler}
+							value={inputState.adress}
 							type="text"
 							placeholder="1137 Williams Avenue"
 						/>
@@ -45,6 +78,9 @@ const CartForm = () => {
 					<div className="cart-form__item">
 						<label>ZIP Code</label>
 						<input
+							name="zip"
+							onChange={inputsHandler}
+							value={inputState.zip}
 							type="text"
 							placeholder="10001"
 						/>
@@ -53,6 +89,9 @@ const CartForm = () => {
 					<div className="cart-form__item">
 						<label>City</label>
 						<input
+							name="city"
+							onChange={inputsHandler}
+							value={inputState.city}
 							type="text"
 							placeholder="New York"
 						/>
@@ -61,6 +100,9 @@ const CartForm = () => {
 					<div className="cart-form__item">
 						<label>Country</label>
 						<input
+							name="country"
+							onChange={inputsHandler}
+							value={inputState.country}
 							type="text"
 							placeholder="United States"
 						/>
@@ -76,7 +118,7 @@ const CartForm = () => {
 						<div className="cart-form__radio-wrapper">
 							<input
 								id="e-money"
-								name="cash-method"
+								name="method"
 								type="radio"
 								placeholder="1137 Williams Avenue"
 							/>
@@ -114,12 +156,12 @@ const CartForm = () => {
 							</label>
 							<input
 								id="cash"
-								name="cash-method"
+								name="method"
 								type="radio"
 								placeholder="1137 Williams Avenue"
 							/>
 							<label for="cash">
-							<div className="checker">
+								<div className="checker">
 									<div className="checker__oval-1">
 										<svg
 											viewBox="0 0 20 20"
@@ -155,6 +197,9 @@ const CartForm = () => {
 					<div className="cart-form__item">
 						<label>e-Money Number</label>
 						<input
+							name="money"
+							onChange={inputsHandler}
+							value={inputState.money}
 							type="text"
 							placeholder="238521993"
 						/>
